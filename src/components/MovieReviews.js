f34da6f7d57e17616523e7d9d21fe344ad40e562
ensallee <ensallee@gmail.com>
2018-06-27 21:15:50 -0400

    done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+  console.log('Movie Reviews props', reviews)
+
+  let reviewList = reviews.map((review) => {
+    return <div className="review">
+      <h1>Title: {review.display_title}</h1>
+      <h3>By: {review.byline}</h3>
+      <p>{review.headline}</p>
+    </div>
+  })
+
+  return (
+    <div className='review-list'>
+      <ul>
+      {reviewList}
+      </ul>
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,34 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews'
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json/query=comedy?'
+            + `api-key=${NYT_API_KEY}`;
+
+
+class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state={
+      reviews: [],
+      searchTerm: 'comedy'
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(resp=> resp.json())
+    .then((reviews) => this.setState({reviews: reviews.results}))
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        return <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer;
